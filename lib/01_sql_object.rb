c6d76e11a3b6074c79199a87797de795833f2745
acatxnamedvirtue <tsb252@gmail.com>
2015-08-14 16:38:29 -0400

    Finish project

--- lib/01_sql_object.rb
+++ lib/01_sql_object.rb
@@ -5,53 +5,104 @@ require 'active_support/inflector'
 
 class SQLObject
   def self.columns
-    # ...
+    (DBConnection.execute2(<<-SQL)
+      SELECT
+        *
+      FROM
+        #{table_name}
+    SQL
+    ).first.map(&:to_sym)
   end
 
   def self.finalize!
+    columns.each do |column|
+      define_method("#{column}") { attributes[column] }
+      define_method("#{column}="){ |value| attributes[column] = value }
+    end
   end
 
   def self.table_name=(table_name)
-    # ...
+    @table_name = table_name
   end
 
   def self.table_name
-    # ...
+    @table_name ||= self.to_s.tableize
   end
 
   def self.all
-    # ...
+    results = DBConnection.execute(<<-SQL)
+      SELECT
+        #{table_name}.*
+      FROM
+        #{table_name}
+    SQL
+
+    parse_all(results)
   end
 
   def self.parse_all(results)
-    # ...
+    results.map { |obj_hash| self.new(obj_hash) }
   end
 
   def self.find(id)
-    # ...
+    result = DBConnection.execute(<<-SQL, id)
+      SELECT
+        #{table_name}.*
+      FROM
+        #{table_name}
+      WHERE
+        id = ?
+    SQL
+
+    parse_all(result).first
   end
 
   def initialize(params = {})
-    # ...
+    params.each do |attr_name, value|
+      unless self.class.columns.include?(attr_name.to_sym)
+        raise "unknown attribute '#{attr_name}'"
+      end
+
+      self.send("#{attr_name}=", value)
+    end
   end
 
   def attributes
-    # ...
+    @attributes ||= {}
   end
 
   def attribute_values
-    # ...
+    attributes.values
   end
 
   def insert
-    # ...
+    col_names = self.class.columns[1..-1].join(", ")
+    question_marks = (["?"] * attribute_values.length).join(", ")
+
+    DBConnection.execute(<<-SQL, *attribute_values)
+      INSERT INTO
+        #{self.class.table_name} (#{col_names})
+      VALUES
+        (#{question_marks})
+    SQL
+
+    self.id = DBConnection.last_insert_row_id
   end
 
   def update
-    # ...
+    set_cols = self.class.columns.map { |attr_name| "#{attr_name} = ?" }.join(", ")
+
+    DBConnection.execute(<<-SQL, *attribute_values, id)
+      UPDATE
+        #{self.class.table_name}
+      SET
+        #{set_cols}
+      WHERE
+        id = ?
+    SQL
   end
 
   def save
-    # ...
+    id.nil? ? insert : update
   end
 end
--- lib/03_associatable.rb
+++ lib/03_associatable.rb
@@ -10,41 +10,66 @@ class AssocOptions
   )
 
   def model_class
-    # ...
+    class_name.constantize
   end
 
   def table_name
-    # ...
+    model_class.table_name
+  end
+
+  def set_defaults(name, self_class_name = nil)
+    self.foreign_key ||= self.is_a?(BelongsToOptions) ? "#{name}_id".to_sym : "#{self_class_name.downcase}_id".to_sym
+    self.class_name ||= self.is_a?(BelongsToOptions) ? "#{name.to_s.capitalize}" : "#{name.to_s.singularize.capitalize}"
+    self.primary_key ||= :id
   end
 end
 
 class BelongsToOptions < AssocOptions
   def initialize(name, options = {})
-    # ...
+    options.each { |attr_name, value| self.send("#{attr_name}=",value) }
+    set_defaults(name)
   end
 end
 
 class HasManyOptions < AssocOptions
   def initialize(name, self_class_name, options = {})
-    # ...
+    options.each { |attr_name, value| self.send("#{attr_name}=",value) }
+    set_defaults(name, self_class_name)
   end
 end
 
 module Associatable
   # Phase IIIb
   def belongs_to(name, options = {})
-    # ...
+    options = BelongsToOptions.new(name, options)
+    assoc_options[name] = options
+
+    define_method(name) do
+      foreign_key = options.send(:foreign_key)
+      target_class = options.model_class
+      primary_key = options.send(:primary_key)
+
+      target_class.where(primary_key => self.send(foreign_key)).first
+    end
   end
 
   def has_many(name, options = {})
-    # ...
+    options = HasManyOptions.new(name, "#{self}", options)
+
+    define_method(name) do
+      foreign_key = options.send(:foreign_key)
+      target_class = options.model_class
+      primary_key = options.send(:primary_key)
+
+      target_class.where(foreign_key => self.send(primary_key))
+    end
   end
 
   def assoc_options
-    # Wait to implement this in Phase IVa. Modify `belongs_to`, too.
+    @assoc_options ||= {}
   end
 end
 
 class SQLObject
-  # Mixin Associatable here...
+  extend Associatable
 end
