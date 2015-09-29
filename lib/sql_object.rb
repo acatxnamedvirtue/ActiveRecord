1289220dfdd0f38be41c51272fca68fc45163cf4
acatxnamedvirtue <tsb252@gmail.com>
2015-09-28 20:53:47 -0400

    organize file structure

--- lib/sql_object.rb
+++ lib/sql_object.rb
@@ -1,9 +1,10 @@
 require_relative 'db_connection'
 require 'active_support/inflector'
-# NB: the attr_accessor we wrote in phase 0 is NOT used in the rest
-# of this project. It was only a warm up.
 
 class SQLObject
+  extend Searchable
+  extend Associatable
+
   def self.columns
     (DBConnection.execute2(<<-SQL)
       SELECT
